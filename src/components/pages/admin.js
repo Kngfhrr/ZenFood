ae0046ea1b03575bc25b7ffd7a69ec4b55c0e870
@nikolay.shutov.kg <nikolay.shutov@zensoft.io>
2019-06-11 18:20:17 +0600

    add modal window

--- src/components/pages/admin.js
+++ src/components/pages/admin.js
@@ -1,13 +1,31 @@
 import React from "react";
 import logo from "../image/Group.svg";
 import "../styles/admin.css";
+import moment from 'moment'
 import Ellipse from '../image/Ellipse.svg';
+import Swal from "sweetalert2";
+
+
 
 export class Admin extends React.Component {
 
- 
+    addItem=()=>{
+        Swal.fire({
+            width: 1000,
+            heightAuto: false,
+            height: 1000,
+            html: '<div style="display: flex; flex-direction: column">' +
+            '<span>Add item</span>' +
+            '<input class="add-item-input"></input>' +
+            '<input class="add-item-price-button"></input>' +
+            '</div>',
+            text: 'Modal with a custom image.',
+            animation: false
+        })
+    }
+
   render() {
-    
+
     return (
       <div>
         <div className="header">
@@ -25,25 +43,25 @@ export class Admin extends React.Component {
         </div>
         <div className="table-header">
           <span className="title-profile">Admin Page</span>
-          <span className="date-today">Monday 18 February</span>
+          <span className="date-today">{moment().format("ddd DD MMM")}</span>
         </div>
         
         <div className="table">
-        <span className='menu-on-today'>menu on 18 February (today)</span>
+        <span className='menu-on-today'>menu on {moment().format("DD MMM")} (today)</span>
         <div className='table-head'>
-          <span className='table-name'>Name</span>
-          <span className='table-price'>Price</span>
+          <span className='table-name'><b>Name</b></span>
+          <span className='table-price'><b>Price</b></span>
           
         </div>
         <div><span className='line'/></div>
         {/*map here  */}
         <div className='table-head'>
-         <span className='table-name'>some food</span>
-          <span className='table-price'>120 som</span>
+         <span className='table-name-admin'>some food</span>
+          <span className='table-price-admin'>120 som</span>
           <span className='close'/>
         </div>
         <div><span className='line'/></div>
-        <button onClick={this.scan} className='table-button'>Add item</button>
+        <button onClick={this.addItem} className='table-button'>Add item</button>
         </div>
       </div>
     );
--- src/components/pages/menu.js
+++ src/components/pages/menu.js
@@ -5,11 +5,12 @@ import Swal from "sweetalert2";
 import Ellipse from "../image/Ellipse.svg";
 
 export class Menu extends React.Component {
-  test = () => {
+  addFood = () => {
     Swal.fire({
       title: "Are you sure in your choose?",
       showCancelButton: true,
       confirmButtonColor: "#0468E7",
+        animation: false,
       cancelButtonColor: "#B4B4B4",
       cancelButtonText: "Nope, go back",
       confirmButtonText: "Yep, confirm!",
@@ -73,9 +74,9 @@ export class Menu extends React.Component {
           </div>
           {/*map here  */}
           <div className="table-head">
-            <span className="table-name">some food</span>
-            <span className="table-price">120 som</span>
-            <button onClick={this.test} className="button-menu">
+            <span className="table-name-menu">some food</span>
+            <span className="table-price-menu">120 som</span>
+            <button onClick={this.addFood} className="button-menu">
               <b>+</b>
             </button>
           </div>
